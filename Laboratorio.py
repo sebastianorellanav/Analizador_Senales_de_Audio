02b2b5d3bb1cb739587d66519d7a85d94b21b188
sebastianorellanav <sebastian.orellana.v@usach.cl>
2020-05-04 21:26:01 -0400

    creacion del archivo laboratorio.py

---
+++ Laboratorio.py
@@ -0,0 +1,10 @@
+#Laboratorio 1 Redes
+
+#1.Lea una señal de audio y determine a qué corresponde cada uno de los parámetros retornados.
+
+#2.Grafique la función de audio en el tiempo.
+
+#3.Calcule la transformada de Fourier de la señal de audio:a.Grafique la señal en el dominio de la frecuencia.b.Al resultado del punto 3, calcule la transformada de Fourier inversa.c.Compare con la señal leída en el punto 1.
+
+#4.Calculeygrafiqueelespectrogramadelafunción.​Elespectrogramapermitevisualizarinformacióneneldominiodelafrecuenciaydeltiempoalavez.Notequerequiere un gráfico de tres dimensiones (t, f, |fft|) o una imagen, ​ver Figura 1
+
