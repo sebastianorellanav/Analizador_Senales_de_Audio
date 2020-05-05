8d48fa82cda7a23d397bd769f590fe003603e2ed
sebastianorellanav <sebastian.orellana.v@usach.cl>
2020-05-04 21:40:24 -0400

    se agregaron comentarios

--- Laboratorio.py
+++ Laboratorio.py
@@ -1,10 +1,34 @@
-#Laboratorio 1 Redes
+###########################################################################################################
+#######---------------------------------------------------------------------------------------------#######
+#######                                   Laboratorio 1 Redes                                       #######
+#######---------------------------------------------------------------------------------------------#######
+#######                                   Sebastian Orellana                                        #######
+#######                                      Franco Tapia                                           #######
+#######                                                                                             #######
+#######                                        version 1                                            #######
+#######---------------------------------------------------------------------------------------------#######
+#######                                       descripcion                                           #######
+###########################################################################################################
 
 #1.Lea una señal de audio y determine a qué corresponde cada uno de los parámetros retornados.
 
+
 #2.Grafique la función de audio en el tiempo.
 
-#3.Calcule la transformada de Fourier de la señal de audio:a.Grafique la señal en el dominio de la frecuencia.b.Al resultado del punto 3, calcule la transformada de Fourier inversa.c.Compare con la señal leída en el punto 1.
 
-#4.Calculeygrafiqueelespectrogramadelafunción.​Elespectrogramapermitevisualizarinformacióneneldominiodelafrecuenciaydeltiempoalavez.Notequerequiere un gráfico de tres dimensiones (t, f, |fft|) o una imagen, ​ver Figura 1
+#3.Calcule la transformada de Fourier de la señal de audio:
+#a.Grafique la señal en el dominio de la frecuencia.
+#b.Al resultado del punto 3, calcule la transformada de Fourier inversa.
+#c.Compare con la señal leída en el punto 1.
 
+
+#4.Calcule y grafique el espectrograma de la función. ​El espectrograma permite visualizar información en el 
+#dominio de la frecuencia y del tiempo a la vez. Note que requiere un gráfico de tres dimensiones (t, f, |fft|)
+# o una imagen
+
+
+#Filtre el ruido de la señal de audio leía en el punto 1, para ello:
+#a.Diseñe un filtro FIR para eliminar el ruido de la señal de audio. Determine el tipo de filtro
+#(pasabajos,pasaaltos,o pasabanda) y determine las frecuencias de corte para este, ver Figura 2.
+#b.Pruebe distintos parámetros al momento de aplicar el filtro.
+#c.Obtenga la transformada de Fourier y el espectrograma de la señal filtrada y analice sus resultados.
