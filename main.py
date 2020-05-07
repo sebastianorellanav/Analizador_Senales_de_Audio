a1b217944499639069b6c2cffd5fc475caccf8b6
sebastianorellanav <sebastian.orellana.v@usach.cl>
2020-05-06 22:14:48 -0400

    Se agrego filtro FIR y señales filtradas

--- main.py
+++ main.py
@@ -15,11 +15,13 @@
 import matplotlib.pyplot as plt
 import scipy.fftpack as fourier
 from scipy import signal
+import scipy.io.wavfile as waves
 import numpy as np
 import pregunta1 as pregunta1
 import pregunta2 as pregunta2
 import pregunta3 as pregunta3
 import pregunta4 as pregunta4
+import pregunta6 as pregunta6
 import otros as otros
 
 
@@ -47,6 +49,23 @@ print(rmse)
 
 #4. graficar Espectrograma
 pregunta4.graficarEspectrograma(sonido,freq)
+
+#6.a. Filtre el ruido de la señal de audio con un Filtro FIR
+taps = 100			#numero de pulsos
+cutoff = 1000		#frecuencia de corte
+filtrado_1 = pregunta6.lowPassFilter(sonido, freq, taps, cutoff, 0) #filtrar soonido
+waves.write('filtrado_1.wav', freq, filtrado_1.astype(np.int16))  #guardar audio filtrado_1
+
+#b.Pruebe distintos parámetros al momento de aplicar el filtro.
+taps = 200			#numero de pulsos
+cutoff = 500		#frecuencia de corte
+filtrado_2 = pregunta6.lowPassFilter(sonido, freq, taps, cutoff, 0) #filtrar soonido
+waves.write('filtrado_2.wav', freq, filtrado_2.astype(np.int16))  #guardar audio filtrado_2
+
+taps = 80			#numero de pulsos
+cutoff = 2000		#frecuencia de corte
+filtrado_3 = pregunta6.lowPassFilter(sonido, freq, taps, cutoff, 0) #filtrar soonido
+waves.write('filtrado_3.wav', freq, filtrado_3.astype(np.int16))  #guardar audio filtrado_3
 
 #Mostrar Graficos
 otros.mostrarGraficos()
---
+++ pregunta6.py
@@ -0,0 +1,20 @@
+from scipy import signal
+
+#Entrada:  Señal original, frecuencia, Nº de taps para usar en el filtro, 
+#          Frecuencia de Corte para el filtro, A lo largo de qué eje aplicar 
+#          el filtrado de pasa bajo.
+#Salida: señal filtrada pasa bajo con la misma dimension que la original
+#Descripcion:
+def lowPassFilter(data, fs, nfilt=80, fw_base=4000, axis=0):
+    # La frecuencia de Nyquist de la señal es la mitad de la frecuencia de muestreo
+    nyq_freq = fs / 2.0
+
+    # Se utiliza un FIR filter con un Hamming window.
+    filt = signal.firwin(nfilt, cutoff=fw_base / nyq_freq, window='hamming')
+
+    # Use lfilter para filtrar con el filtro FIR.
+    # Filtramos a lo largo de la segunda dimensión porque eso representa el tiempo
+    filtered_sound = signal.filtfilt(filt, [1.0], data, axis=axis)
+
+    return filtered_sound
+
